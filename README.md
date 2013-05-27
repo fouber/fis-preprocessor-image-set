2a106279ed11a244c3294680190d5424c44ed3af
zhangyunlong <zhangyunlong@baidu.com>
2013-05-28 01:28:27 +0800

    发布image-set插件

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # fis-preprocessor-image-set
 
 A preprocessor plugin for fis to automatically add -webkit-image-set css rule.
+
+## usage
+
+    $ npm install -g fis-preprocessor-image-set
+    $ vi path/to/project/fis-conf.js
+
+```javascript
+//file : path/to/project/fis-conf.js
+fis.config.merge({
+    modules : {
+        preprocessor : {
+            css : 'image-set'
+        }
+    }
+});
+```
--- index.js
+++ index.js
@@ -4,6 +4,16 @@
  */
 
 'use strict';
-module.exports = function(content, file, conf){
-    
+module.exports = function(content, file){
+    var reg = /\/\*[\s\S]+?(?=\*\/|$)|(?:\{|;|\*\/)(\s*)background(?:-image)?\s*:\s*[^;\}]*?url\s*\(([^\)]+)\)(?:[^;\}/]|\/[^*]|\/\*[\s\S]+?(?:\*\/|$))*/ig;
+    return content.replace(reg, function(m, blank_space, value){
+        if(value){
+            var retina = value.replace(/\.[^.\/\\]+$/, '_2x$&');
+            var info = fis.uri(retina, file.dirname);
+            if(info.file){
+                m += ';' + blank_space + 'background-image:-webkit-image-set(url(' + value + ') 1x, url(' + retina + ') 2x)';
+            }
+        }
+        return m;
+    });
 };
